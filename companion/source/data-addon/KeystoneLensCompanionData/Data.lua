d33e6ce4e12b4d5e1523293e2cf678943abad028
Yolol100 <Andrew_Baeten@hotmail.com>
2026-08-18 20:42:38 +0200

    Isolate schema-v2 tooltip cache

--- companion/source/data-addon/KeystoneLensCompanionData/Data.lua
+++ companion/source/data-addon/KeystoneLensCompanionData/Data.lua
@@ -1,6 +1,7 @@
 -- Generated/overwritten by KeystoneLens Companion. Ships empty by design.
 -- Public character statistics only. Never place API credentials in this file.
-KeystoneLensTooltipCache = {
+_G.KeystoneLensTooltipCache = nil
+_G.KeystoneLensTooltipCacheV2 = {
     version = 2,
     generatedAt = 0,
     maxAge = 43200,
