89a7d31a757a24b3bd35e98a0f6c1646e1ee48d8
Andrew Baeten <Andrew_Baeten@hotmail.com>
2026-09-10 23:10:00 +0200

    Install recruitment preference persistence bridge

--- companion/source/app/keystonelens_companion/__init__.py
+++ companion/source/app/keystonelens_companion/__init__.py
@@ -9,3 +9,8 @@ from .ui_recruitment_patch import install as _install_ui_recruitment_patch
 
 _install_ui_recruitment_patch()
 del _install_ui_recruitment_patch
+
+from .ui_recruitment_persistence_patch import install as _install_ui_recruitment_persistence_patch
+
+_install_ui_recruitment_persistence_patch()
+del _install_ui_recruitment_persistence_patch
