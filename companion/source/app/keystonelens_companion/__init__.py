6973cb9063a776299c0aa74d7e7e2600817e4760
Andrew Baeten <Andrew_Baeten@hotmail.com>
2026-09-10 23:08:26 +0200

    Install recruitment UI extension

--- companion/source/app/keystonelens_companion/__init__.py
+++ companion/source/app/keystonelens_companion/__init__.py
@@ -4,3 +4,8 @@ from .ui_layout_patch import install as _install_ui_layout_patch
 
 _install_ui_layout_patch()
 del _install_ui_layout_patch
+
+from .ui_recruitment_patch import install as _install_ui_recruitment_patch
+
+_install_ui_recruitment_patch()
+del _install_ui_recruitment_patch
