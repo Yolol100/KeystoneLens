2ac06b1b065d905a32e35fa7bed0002ae21d88d4
Andrew Baeten <Andrew_Baeten@hotmail.com>
2026-09-25 06:32:22 +0200

    Remove obsolete UI patch bootstrap

--- companion/source/app/keystonelens_companion/__init__.py
+++ companion/source/app/keystonelens_companion/__init__.py
@@ -1,16 +1,1 @@
-__version__ = "0.13.0"
-
-from .ui_layout_patch import install as _install_ui_layout_patch
-
-_install_ui_layout_patch()
-del _install_ui_layout_patch
-
-from .ui_recruitment_patch import install as _install_ui_recruitment_patch
-
-_install_ui_recruitment_patch()
-del _install_ui_recruitment_patch
-
-from .ui_recruitment_persistence_patch import install as _install_ui_recruitment_persistence_patch
-
-_install_ui_recruitment_persistence_patch()
-del _install_ui_recruitment_persistence_patch
+__version__ = "0.13.1"
